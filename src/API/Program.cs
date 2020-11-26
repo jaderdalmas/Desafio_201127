8eec4f0944e9eebd0f10714d5b704f93dd52ba9b
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-26 20:51:52 -0300

    add timed hosted service

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,4 +1,6 @@
+using API.Service;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace API
@@ -11,6 +13,7 @@ namespace API
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
-      .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+      .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+      .ConfigureServices(services => services.AddHostedService<TimedHostedService>());
   }
 }
---
+++ src/API/Service/TimedHostedService.cs
@@ -0,0 +1,66 @@
+﻿using API.Repository;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Service
+{
+  public class TimedHostedService : IHostedService, IDisposable
+  {
+    private int executionCount = 0;
+    private readonly ILogger<TimedHostedService> _logger;
+    private Timer _timer;
+
+    private IItemService _itemService;
+    private IItemRepository _itemRepository;
+
+    public TimedHostedService(ILogger<TimedHostedService> logger, IItemService itemService, IItemRepository itemRepository)
+    {
+      _logger = logger;
+
+      _itemService = itemService;
+      _itemRepository = itemRepository;
+    }
+
+    public Task StartAsync(CancellationToken stoppingToken)
+    {
+      _logger.LogInformation("Timed Hosted Service running.");
+
+      _timer = new Timer(DoWork, null, TimeSpan.Zero,
+          TimeSpan.FromMinutes(2));
+
+      return Task.CompletedTask;
+    }
+
+    private void DoWork(object state)
+    {
+      var count = Interlocked.Increment(ref executionCount);
+
+      var watch = new Stopwatch();
+      watch.Start();
+      var result = _itemRepository.LIFO().Result;
+      if (result != null)
+        _itemService.PostCSV(result).Wait();
+      watch.Stop();
+
+      _logger.LogInformation("Timed Hosted Service is working. Count: {Count} | Time: {Elapsed}", count, watch.Elapsed);
+    }
+
+    public Task StopAsync(CancellationToken stoppingToken)
+    {
+      _logger.LogInformation("Timed Hosted Service is stopping.");
+
+      _timer?.Change(Timeout.Infinite, 0);
+
+      return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+      _timer?.Dispose();
+    }
+  }
+}
