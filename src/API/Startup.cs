bd1dfa51f0f0f63c3a9c21024cf61a0215371f79
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-26 14:01:22 -0300

    add repos, swagger and health on startup

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -1,3 +1,5 @@
+using Api.StartUp;
+using API.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,6 +21,10 @@ namespace API
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllers();
+      services.AddRepositories();
+
+      services.AddSwagger();
+      services.AddHealthCheck();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,9 +35,13 @@ namespace API
         app.UseDeveloperExceptionPage();
       }
 
+      app.ConfigureSwagger();
+      app.ConfigureHealthCheck();
+
       app.UseHttpsRedirection();
       app.UseRouting();
       app.UseAuthorization();
+
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
