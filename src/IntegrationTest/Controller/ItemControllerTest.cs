f6412d5202e5aa321e35744564451523f0b273b2
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-26 19:33:48 -0300

    test wrinting new csv

--- src/IntegrationTest/Controller/ItemControllerTest.cs
+++ src/IntegrationTest/Controller/ItemControllerTest.cs
@@ -1,7 +1,9 @@
 using API;
 using API.Extension;
 using API.Model;
+using API.Service;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Net;
@@ -15,9 +17,15 @@ namespace IntegrationTest.Controller
   {
     private readonly TestApplicationFactory<Startup> _factory;
 
+    private readonly IServiceScope _serviceScope;
+    private IServiceProvider ServiceProvider => _serviceScope.ServiceProvider;
+
+    private IItemService Service => ServiceProvider.GetService<IItemService>();
+
     public ItemControllerTest(TestApplicationFactory<Startup> factory)
     {
       _factory = factory;
+      _serviceScope = factory.Services.CreateScope();
     }
 
     [Fact]
@@ -195,6 +203,46 @@ namespace IntegrationTest.Controller
       Assert.Equal(request[2].Moeda, item.Moeda);
       Assert.Equal(request[2].DataInicio, item.DataInicio);
       Assert.Equal(request[2].DataFim, item.DataFim);
+    }
+
+    [Fact]
+    public async Task GetItem()
+    {
+      // Arrange
+      var client = _factory.CreateClient();
+
+      var request = new List<Item>() {
+        new Item(){
+          Moeda = "USD",
+          DataInicio = DateTime.Parse("2010-01-01"),
+          DataFim = DateTime.Parse("2010-12-01")
+        },
+        new Item()
+        {
+          Moeda = "EUR",
+          DataInicio = DateTime.Parse("2020-01-01"),
+          DataFim = DateTime.Parse("2020-12-01")
+        },
+        new Item()
+        {
+          Moeda = "JPY",
+          DataInicio = DateTime.Parse("2000-03-11"),
+          DataFim = DateTime.Parse("2000-03-30")
+        }
+      };
+      _ = await client.PostAsync("/Item", request.AsContent());
+
+      // Act
+      var response = await client.GetAsync("/Item");
+
+      // Assert
+      Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+      var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+      Assert.NotEmpty(result);
+
+      var item = JsonSerializer.Deserialize<Item>(result);
+      Service.GetItem(item).Wait();
     }
   }
 }
---
+++ src/IntegrationTest/Service/CSVServiceTest.cs
@@ -0,0 +1,67 @@
+﻿using API.Model;
+using API.Service;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTest.Service
+{
+  public class CSVServiceTest
+  {
+    [Fact]
+    public async Task GetCotacao()
+    {
+      // Arrange
+      var log = new Mock<ILogger<CSVService>>();
+      var service = new CSVService(log.Object);
+
+      // Act
+      var cotacao = await service.GetCotacao().ConfigureAwait(false);
+
+      // Assert
+      Assert.NotEmpty(cotacao);
+    }
+
+    [Fact]
+    public async Task GetMoeda()
+    {
+      // Arrange
+      var log = new Mock<ILogger<CSVService>>();
+      var service = new CSVService(log.Object);
+
+      // Act
+      var moeda = await service.GetMoeda().ConfigureAwait(false);
+
+      // Assert
+      Assert.NotEmpty(moeda);
+    }
+
+    [Fact]
+    public async Task PostMoedaCotacao()
+    {
+      // Arrange
+      var log = new Mock<ILogger<CSVService>>();
+      var service = new CSVService(log.Object);
+
+      IList<MoedaCotacao> list = new List<MoedaCotacao>()
+      {
+        new MoedaCotacao()
+        {
+          Id = "USD",
+          Data = DateTime.Now.Date,
+          Codigo = 1,
+          Valor = (decimal)1.69
+        }
+      };
+
+      // Act
+      await service.PostMoedaCotacao(list).ConfigureAwait(false);
+
+      // Assert
+      Assert.NotEmpty(list);
+    }
+  }
+}
